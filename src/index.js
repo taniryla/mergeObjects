6a7b670813845acc271ebc96f2deaab7e05829fb
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-02 21:29:23 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 15-mergeObjects
+Difficulty:  Intermediate
+Prompt:
+- Write a function named mergeObjects that accepts at least two objects as arguments, merges the properties of the second through n objects into the first object, then finally returns the first object.
+- If any objects have the same property key, values from the object(s) later in the arguments list should overwrite earlier values.
+Examples:
+mergeObjects({}, {a: 1});  //=> {a: 1} (same object as first arg)
+mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  //=> {a: 1, b: 2, c: 3, d: 4}
+mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44}
+-----------------------------------------------------------------*/
+// Your solution for 15-mergeObjects here:
+
+</div>
+`;
+
+function mergeObjects(target, ...obj) {
+  return Object.assign(target, ...obj);
+}
